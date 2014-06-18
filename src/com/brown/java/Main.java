c2340f16f32f36df4c5b294833b80785245dd47b
Evaaan <ebrown@localhost.localdomain>
2014-06-17 22:06:36 -0700

    Updated to add proper regex parsing

--- src/com/brown/java/Main.java
+++ src/com/brown/java/Main.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 	
@@ -12,8 +14,11 @@ public class Main {
 	static int lineNum=0;
 	static ArrayList<Integer> startIndex = new ArrayList<Integer>();
 	static ArrayList<Integer> endIndex = new ArrayList<Integer>();
-	static ArrayList<String> equationValue = new ArrayList<String>();
-	static String equation ="";
+	static ArrayList<String> equation = new ArrayList<String>();
+	static Pattern pEq = Pattern.compile("(\\s+)?(([0-9]+)?(_)?[a-zA-Z]+(_)?([0-9]+)?)\\s+?(.*)");	
+	
+	
+	//static String equation ="";
 	
 	public static void main(String[] args){
 		
@@ -32,35 +37,39 @@ public class Main {
 			while(scanner.hasNext()){
 				lineNum++;
 				//System.out.println(scanner.next());
-				String line = scanner.next();
+				String line = scanner.next();				
 				
+				if(line.contains("Equation"))continue;					
+				if(line.trim().isEmpty())continue;
 				
-				if(line.contains("Equation:")){
+				Matcher matcher = pEq.matcher(line);
+				if(matcher.find()){
+					System.out.println(line);
 					
-					if(!LadderLogicEquation.isEquation(line)){						
-						endIndex.add(lineNum);
-						equationValue.add(LadderLogicEquation.getEquation(line));
-						LadderLogicEquation.unsetEquation();
-						continue;
+				}
+				else{
+					System.out.println(line);
+				}
+				//System.out.println(line);
+				
+				
+				/*String[] expression =line.split("/[a-zA-z]+/");
+				
+				System.out.println(expression.length);
+				
+				for(int i =0; i<expression.length;i++){
+					if(!expression[i].toString().isEmpty()){
+						equation.add(expression[i].toString());
+						System.out.print(expression[i].toString()+ " ");
 					}
 					
-					if(LadderLogicEquation.isEquation(line)){
-						LadderLogicEquation.setEquation(line);
-						
-						startIndex.add(lineNum);					
-					}
-					
-				
 				}
 				
 				
-				
+				System.out.println();*/
 			}
 			
-			for(int i=0; i<equationValue.size();i++){
-				
-				System.out.println("Equation " + equationValue.toArray()[i] + " is between " + startIndex.toArray()[i] + " " + endIndex.toArray()[i]);
-			}
+			
 			
 			
 	}
